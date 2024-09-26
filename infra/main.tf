8fd3176034558537b15c400ea1eeb9dfe7b48eef
Rinshad rabab <n286535@pagonxt.com>
2024-09-26 08:55:30 +0200

    Modified workflow file

---
+++ infra/main.tf
@@ -0,0 +1,25 @@
+terraform {
+          required_providers {
+            kind = {
+              source  = "tehcyx/kind"
+              version = "0.2.1"
+            }
+          }
+        }
+
+        resource "kind_cluster" "default" {
+          name = "test-cluster"
+          kind_config {
+            kind = "Cluster"
+            api_version = "kind.x-k8s.io/v1alpha4"
+            node {
+              role = "control-plane"
+            }
+            node {
+              role = "worker"
+            }
+            node {
+              role = "worker"
+            }
+          }
+        }
