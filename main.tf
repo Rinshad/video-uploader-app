9cecc043bc84791cb9ec7f7ade3e1ec528e46312
Rinshad rabab <n286535@pagonxt.com>
2024-09-28 16:02:19 +0200

    App deployment Test

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "video_uploader" {
+  name             = "video-uploader"
+  namespace        = "video-uploader"
+  create_namespace = true
+  chart            = "${path.root}/../Helm_Chart"
+
+  values = [
+    file("${path.module}/../Helm_Chart/values.yaml")
+  ]
+
+  set {
+    name  = "image.tag"
+    value = "latest"
+  } 
+}
